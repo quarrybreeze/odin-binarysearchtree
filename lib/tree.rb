93dcb35954a62758c2ba455a65945d792a892a2d
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-27 19:58:19 -0800

    finished removing nodes with 2 children.
    to do: write find, level_order, depth first, and other methods.

--- lib/tree.rb
+++ lib/tree.rb
@@ -73,10 +73,6 @@ class Tree
       child_count = 1
     end
 
-    p "parent: #{parent.data}"
-    p "current: #{current.data}"
-
-    
     if input != current.data
       p "#{input} is not a node"
     elsif child_count == 0
@@ -94,54 +90,28 @@ class Tree
       elsif current.right
         parent.left = current.right
       end
+    elsif child_count == 2
+      next_biggest = nil  #keep track of next biggest
+      node_to_replace = current #keep track the node we are trying to replace
+
+      parent = current
+      current = current.right
+
+      next_biggest = current
+      while current.left
+        parent = current #keep track of parent of next_biggest
+        current = current.left
+        next_biggest = current
+      end
+      node_to_replace.data = next_biggest.data
+      if node_to_replace.right == next_biggest  #if the right node is the next biggest,
+        node_to_replace.right = next_biggest.right #then replace it
+      else
+        parent.left = next_biggest.right  #otherwise, replace the parent of the next_biggest
+      end
     else
-      p "not yet built"
+      p "error"
     end
-
-
-    
-
-    # if child_count == 1 && 
-    #   if current.left && input < current.left.data
-    #     parent.left = current.left
-    #   elsif current.right && input > current.right.data
-    #     parent.right = current.right
-    #   else
-    #     p "big error deleting #{input}"
-    #   end
-    # end
-      
-    # elsif child_count == 1
-    #   if current.left
-    #     parent.right = current.left
-    #   elsif current.right
-    #     parent.left = current.right
-    #   else
-    #     p "big error"
-    #   end
-
-
-    # if input == current.data
-    #   p "#{input} ready to be deleted!"
-    #   current = nil
-    # elsif input < current.data
-    #   current.left = Node.new(input)
-    # elsif input > current.data
-    #   current.right = Node.new(input)
-    # else
-    #   "error"
-    # end
-
-    #if the node to be deleted is a leaf (no childen) 
-    #delete the leaf
-    #if the node has 1 child
-    #replace the deleted node with the child
-    #if the node has 2 children
-    #1. update data with next biggest (go right and left until no lefts)
-    #2a. if next biggest was a leaf, done
-    #2b. if next biggest has children, replace the node with the child.
-
-
   end
   
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -155,13 +125,17 @@ end
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 test = Tree.new(array)
-test.insert(2)
+test.pretty_print
+# test.insert(2)
 # test.pretty_print
-test.insert(4)
-test.insert(6)
+# test.insert(4)
+# test.insert(6)
 # test.pretty_print
 # test.delete(2)
 # test.delete(3)
-# test.delete(67)
+
 # test.delete(324)
+# test.delete(67)
+# test.delete(8)
+
 test.pretty_print
