0d0588023871c5b0d92348b0d3d5c987a4912e0d
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-28 01:48:45 -0800

    to do: add height, depth, balanced? and rebalance method

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,6 +2,8 @@ require_relative 'node'
 
 class Tree
 
+  attr_accessor :root
+
   def initialize(array)
     cleaned = self.clean(array)
     @root = self.build_tree(cleaned)
@@ -114,6 +116,133 @@ class Tree
     end
   end
   
+  def level_order
+    value_array = []
+    node_array = []
+    stack = [@root]
+
+    while stack != []
+      node_array << stack[0]
+      value_array << stack[0].data
+      if stack[0].left
+        stack << stack[0].left
+      end
+      if stack[0].right
+        stack << stack[0].right
+      end
+      stack.shift
+    end
+
+    if block_given?
+      node_array.each do |node|
+        yield node
+      end
+    else
+    return value_array
+    end
+  end
+
+  def find(value)
+    current = @root
+
+    while current && current.data != value
+      if value < current.data
+        current = current.left
+      else
+        current = current.right
+      end
+    end
+
+    return current
+  end
+
+  def postorder
+    result = []
+    value_array = []
+    postorder_rec(@root,result)
+    
+    if block_given?
+      result.each do |node|
+      yield node
+      end
+    else
+      result.each do |value|
+        value_array << value.data
+      end
+    end
+    return value_array
+  end
+
+  def inorder
+    result = []
+    value_array = []
+    inorder_rec(@root,result)
+    
+    if block_given?
+      result.each do |node|
+      yield node
+      end
+    else
+      result.each do |value|
+        value_array << value.data
+      end
+    end
+    return value_array
+  end
+
+  def preorder
+    result = []
+    value_array = []
+    preorder_rec(@root,result)
+    
+    if block_given?
+      result.each do |node|
+      yield node
+      end
+    else
+      result.each do |value|
+        value_array << value.data
+      end
+    end
+    return value_array
+  end
+
+  def inorder_rec(root,result)
+    if root
+      if root.left
+        inorder_rec(root.left, result)
+      end
+      result << root
+      if root.right
+        inorder_rec(root.right,result)
+      end
+    end
+  end
+
+  def postorder_rec(root,result)
+    if root
+      if root.left
+        postorder_rec(root.left, result)
+      end
+      if root.right
+        postorder_rec(root.right,result)
+      end
+      result << root
+    end
+  end
+
+  def preorder_rec(root, result)
+    if root
+      result << root
+      if root.left
+        preorder_rec(root.left, result)
+      end
+      if root.right
+        preorder_rec(root.right, result)
+      end
+    end
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -123,7 +252,8 @@ class Tree
 end
 
 
-array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+# array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+array = [1,2,3,4,5,6,7,9,10,11]
 test = Tree.new(array)
 test.pretty_print
 # test.insert(2)
@@ -138,4 +268,10 @@ test.pretty_print
 # test.delete(67)
 # test.delete(8)
 
-test.pretty_print
+# test.level_order {|node| puts "Visited node: #{node}"}
+
+# temp = test.level_order
+# p temp
+p test.preorder
+p test.inorder
+p test.postorder
